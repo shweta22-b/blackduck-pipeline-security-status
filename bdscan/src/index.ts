90a97c84250218f8d4039cc3e5d1c0e74033aaff
shweta22-b <Sbajpai@allegion.com>
2026-01-13 14:30:46 +0530

    Convert Azure DevOps task to GitHub Action

--- bdscan/src/index.ts
+++ bdscan/src/index.ts
@@ -1,106 +1,80 @@
-import * as task from 'azure-pipelines-task-lib/task';
-import { DetectADOConstants } from './lib/BlackDuckConstants';
-import {IBlackDuckConfig} from './models/IBlackDuckConfig';
-import { IBlackDuckToken } from './models/IBlackDuckToken';
-import { IBlackDuckProject } from './models/IBlackDuckProject';
+import * as core from '@actions/core';
 import { IRiskState } from './models/IRiskState';
 import { IBlackDuckVersion } from './models/IBlackDuckVersion';
 import { BlackDuckCheck } from './services/BlackDuckCheck';
-import { BlackDuckAPICalls } from './services/BlackDuckAPICalls'
 
 async function run() {
     try {
-        const bdService = task.getInput('blackduckconnection', false);
-        const bdTkn = task.getInput('blackducktoken', false);
-        const bdProjectName = task.getInput('projectName', true);
-        const bdVersionName = task.getInput('versionName', true);
-        const baseUrl = "allegion.blackducksoftware.com";
-        let blackduckCheck: BlackDuckCheck;
+        // Get inputs from action.yml
+        const bdUrl = core.getInput('blackduck-url', { required: false }) || 'allegion.blackducksoftware.com';
+        const bdToken = core.getInput('blackduck-token', { required: true });
+        const bdProjectName = core.getInput('project-name', { required: true });
+        const bdVersionName = core.getInput('version-name', { required: true });
         
-        if (bdService === undefined && bdTkn === undefined){
-            task.setResult(task.TaskResult.Failed, 'Need Black Duck connection string or token');
-            return
-        }
-
-        /* Get Black Duck Token from Service Connection*/
-        else if(bdTkn === undefined && typeof bdService !== undefined) {
-            let bdCreds: IBlackDuckConfig = await getBlackDuckCredentials(bdService);
-            task.setSecret(bdCreds.blackduckApiToken);
-            blackduckCheck = new BlackDuckCheck(bdCreds.blackduckApiToken, bdProjectName, bdVersionName, bdCreds.blackduckUrl);
-        }
+        // Mask the token in logs
+        core.setSecret(bdToken);
         
-        else if (bdService === undefined && typeof bdTkn !== undefined) {
-            task.setSecret(bdTkn);
-            blackduckCheck = new BlackDuckCheck(bdTkn, bdProjectName, bdVersionName, baseUrl);
-        }
+        // Initialize Black Duck check
+        const blackduckCheck = new BlackDuckCheck(bdToken, bdProjectName, bdVersionName, bdUrl);
+        
         /* Run BlackDuck API Calls */
         let blackDuckData: IBlackDuckVersion = await blackduckCheck.callBlackDuckAPI();
         
         /* Check licenses */
-        const failOnLicenseSelection = task.getBoolInput('failOnLicenseRisks', false);
-        const licenseExclusionsList = task.getInput('licenseExclusions', false);
-        let licenseList = licenseExclusionsList === undefined ? [] : licenseExclusionsList.split(', ');
+        const failOnLicenseSelection = core.getBooleanInput('fail-on-license-risks', { required: false });
+        const licenseExclusionsList = core.getInput('license-exclusions', { required: false });
+        let licenseList = licenseExclusionsList === '' ? [] : licenseExclusionsList.split(', ');
         if (failOnLicenseSelection){
             const licenseCheck = await blackduckCheck.failOnLicenseRisks(blackDuckData, licenseList);
             licenseCheck.forEach((riskAssessment) => {
                 if (riskAssessment.risk)
                 {
-                    task.setResult(task.TaskResult.Failed, riskAssessment.message, true);
+                    core.setFailed(riskAssessment.message);
                 }
                 else {
-                    task.setResult(task.TaskResult.Succeeded, riskAssessment.message)
+                    core.info(riskAssessment.message);
                 }
             });
         }
 
         /* Security check*/
-        const failOnSecuritySelection = task.getBoolInput('failOnSecurityRisks', false);
-        const securityExclusionList = task.getInput('securityExclusions', false);
-        let securityList = securityExclusionList === undefined ? [] : securityExclusionList.split(', ');
+        const failOnSecuritySelection = core.getBooleanInput('fail-on-security-risks', { required: false });
+        const securityExclusionList = core.getInput('security-exclusions', { required: false });
+        let securityList = securityExclusionList === '' ? [] : securityExclusionList.split(', ');
         if (failOnSecuritySelection){
             let securityCheck: IRiskState[] = await blackduckCheck.failOnSecurityRisks(blackDuckData, securityList);
             securityCheck.forEach((riskAssessment) => {
                 if (riskAssessment.risk){
-                    task.setResult(task.TaskResult.Failed, riskAssessment.message);
+                    core.setFailed(riskAssessment.message);
                 }
                 else{
-                    task.setResult(task.TaskResult.Succeeded, riskAssessment.message)
+                    core.info(riskAssessment.message);
                 }
             });
         }
 
         /* Policy check */
-        const failOnPolicySelection = task.getBoolInput('failOnPolicyViolations', false);
-        const policyExclusionList = task.getInput('policyExclusions', false);
-        let policyList = policyExclusionList === undefined ? [] : policyExclusionList.split(', ');
+        const failOnPolicySelection = core.getBooleanInput('fail-on-policy-violations', { required: false });
+        const policyExclusionList = core.getInput('policy-exclusions', { required: false });
+        let policyList = policyExclusionList === '' ? [] : policyExclusionList.split(', ');
         if (failOnPolicySelection){
             let policyCheck:IRiskState[] = await blackduckCheck.failOnPolicyViolations(blackDuckData, policyList);
             policyCheck.forEach((riskAssessment) => {
                 if (riskAssessment.risk)
                 {
-                    task.setResult(task.TaskResult.Failed, riskAssessment.message);
+                    core.setFailed(riskAssessment.message);
                 }
                 else
                 {
-                    task.setResult(task.TaskResult.Succeeded, riskAssessment.message)
+                    core.info(riskAssessment.message);
                 }
             })
         }
-        task.setResult(task.TaskResult.Succeeded, "Black Duck scan complete. No checks failed.")
+        core.info("Black Duck scan complete. No checks failed.");
     }
     catch (err) {
-        task.setResult(task.TaskResult.Failed, err.message);
+        core.setFailed(err.message);
     }
 }
 
 run();
-
-async function getBlackDuckCredentials(bdService): Promise < IBlackDuckConfig > {
-    const bdUrl: string = task.getEndpointUrl(bdService, false);
-    const bdToken: string = task.getEndpointAuthorizationParameter(bdService, DetectADOConstants.BLACKDUCK_API_TOKEN, false);
-
-    return {
-        blackduckUrl: bdUrl,
-        blackduckApiToken: bdToken
-    }
-}
